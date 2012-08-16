022a05f7b9e2a7285e7e5d12a26a4c08f8127894
Sumin Byeon <suminb@gmail.com>
2012-08-16 01:32:19 -0700

    Use SQLite3 to manage URLs.

--- run.py
+++ run.py
@@ -2,7 +2,12 @@ from spider import *
 from proxy import *
 from multiprocessing import Pool
 
+import hashlib
+
+
 proxy = Proxy('http', '79.170.50.25', 80)
+db = Database('spider.db')
+db.connect()
 
 urls = ('http://docs.python.org/library/multiprocessing',
 	'http://stackoverflow.com/questions/5620263/using-an-http-proxy-python',
@@ -10,14 +15,25 @@ urls = ('http://docs.python.org/library/multiprocessing',
 	'http://news.cnet.com/',
 	'http://www.python.org/dev/peps/pep-0257/')
 
-def f(url):
-	task = FetchTask(url)
-	content = task.run(proxy)
+def hash_url(url):
+	return hashlib.sha1(url).hexdigest()
 
-	return len(content)
+def fetch(url):
+	task = FetchTask(url)
+	content = task.run(proxy, db)
+	doc = Document(content)
+	
+	file_name = 'docs/%s.html' % hash_url(url)
+	with open(file_name, 'w') as f:
+		f.write(content.encode('utf-8'))
 
 # This is about 2.5 times faster than the non-parallel method
-pool = Pool(processes=4)
-print pool.map(f, urls)
+#pool = Pool(processes=4)
+#print pool.map(f, urls)
 
 #print map(f, urls)
+
+#doc = Document(open('sample.html').read())
+#print doc.extract_urls()
+
+fetch("http://messages.finance.yahoo.com/Business_%26_Finance/Investments/Stocks_%28A_to_Z%29/Stocks_L/threadview?bn=76474&tid=35875&mid=35912")
--- spider.py
+++ spider.py
@@ -1,16 +1,79 @@
 from proxy import *
+
 import urllib2
+import re
+import sqlite3
+import datetime
+
+class Database:
+
+    def __init__(self, file_name):
+        """
+        file_name -- Name of sqlite3 database file.
+        """
+        self.file_name = file_name
+
+    def __enter__(self):
+        self.connect()
+
+    def __exit__(self):
+        self.close()
+
+    @property
+    def cursor(self):
+        return self.conn.cursor()
+
+    @property
+    def connection(self):
+        return self.conn
+
+    def connect(self):
+        self.conn = sqlite3.connect(self.file_name)
+        return self.conn
+
+    def close(self):
+        self.conn.close()
+
+    def execute(self, query, commit=True):
+        curs = self.conn.cursor()
+        curs.execute(query)
+
+        if commit:
+            self.conn.commit()
+
+    def commit(self):
+        self.conn.commit()
+
+    #
+    # Spider specific functions
+    #
+
+    def has_url(self):
+        curs = self.cursor
+        curs.execute("SELECT * FROM url WHERE url=?", url)
+        row = curs.fetchone()
+
+        return row != None
+
+    def insert_url(self, url, mimetype, timestamp=datetime.datetime.now(), commit=True):
+        curs = self.cursor
+        curs.execute("INSERT INTO url VALUES (?, ?, ?)", (url, mimetype, timestamp))
+
+        if commit: self.commit()
+
 
 class FetchTask:
     
     USER_AGENT = 'Spider v0.1'
     REQUEST_TIMEOUT = 10
 
+    DB_FILE_NAME = 'spider.db'
+
     def __init__(self, url):
         self.url = url
 
-    def run(self, proxy=None):
-        return FetchTask.fetch_url(self.url, proxy)
+    def run(self, proxy=None, db=None):
+        return FetchTask.fetch_url(self.url, proxy, db)
 
     @staticmethod
     def open_url(url, proxy=None):
@@ -23,7 +86,11 @@ class FetchTask:
         return opener.open(url, timeout=FetchTask.REQUEST_TIMEOUT)
 
     @staticmethod
-    def fetch_url(url, proxy=None):
+    def fetch_url(url, proxy=None, db=None):
+
+        if isinstance(db, Database):
+            if db.has_url(url):
+                return None
 
         start_time = time.time()
         content = None
@@ -34,6 +101,7 @@ class FetchTask:
             content = f.read().decode('utf-8')
             f.close()
             succeeded = True
+
         except Exception, e:
             raise e
         finally:
@@ -43,7 +111,11 @@ class FetchTask:
             if isinstance(proxy, Proxy):
                 proxy.report_status(succeeded, time_elapsed)
 
+            if isinstance(db, Database):
+                db.insert_url(url, '')
+
         return content
+
 
 class TaskDispatcher:
     def dispatch(self, task):
@@ -51,3 +123,27 @@ class TaskDispatcher:
         task -- an instance of FetchTask
         """
         raise Exception('Not implemented')
+
+
+class Document:
+
+    # FIXME: This does not cover all possible forms of URLs, but we'll
+    # stick with this for now.
+    url_pattern = r"https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.\-\+]*\/?"
+
+    def __init__(self, raw_content):
+        self.raw_content = raw_content
+
+    def extract_urls(self, url_pattern=None):
+        """Returns a list of HTTP/S URLs in string format."""
+
+        if url_pattern == None:
+            url_pattern = self.url_pattern
+
+        return re.findall(url_pattern, self.raw_content)
+
+
+
+# TODO: Implement URL extractor
+# TODO: Implement URL filter
+# TODO: Implement Write class that abstracts the process of storing fetched pages
