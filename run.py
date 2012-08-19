9a13a13e4471b915c465dccb7eb3194c751c0e45
Sumin Byeon <suminb@gmail.com>
2012-08-19 03:05:47 -0700

    Nicer status report interface.

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ from multiprocessing.pool import ThreadPool
 
 import re
 import random
+import thread
+import threading
+import curses
 
 DB_URL = 'spider.db'
 URL_PATTERNS = (
@@ -12,27 +15,63 @@ URL_PATTERNS = (
     r"http://messages.finance.yahoo.com/search[\/\w %=;&\.\-\+\?]*\/?",
 )
 
-#proxy = Proxy('http', '79.170.50.25', 80)
+def fetch_unfetched_urls(limit):
+    with Database(DB_URL) as db:
+        curs = db.cursor
+        curs.execute("SELECT url FROM document WHERE last_fetched IS NULL LIMIT ?", (limit,))
+        
+        return map(lambda u: u[0], curs.fetchall())
+
+#
+# Shared resources
+#
+
+# number of processes
+n_proc = 8
+
+# number of urls to fetch
+n_urls = 20
+
+unfetched_urls = fetch_unfetched_urls(n_urls)
 
 proxy_list = None
+
+status = {'processed_urls_count':0}
+thread_status = {}
+lock = threading.Lock()
+screen = curses.initscr()
+
 
 def load_proxy_list(file_name):
     with open(file_name) as f:
         return re.findall(r"(https?):\/\/([0-9a-z\.]+):(\d+)", f.read())
 
 def fetch_url(url, thread_seq=0):
+    # thread ID
+    tid = thread.get_ident()
+
+    # randomly choose a proxy server
+    pxy = random.choice(proxy_list)
+    proxy = Proxy(pxy[0], pxy[1], pxy[2])
+
+    lock.acquire()
+    if not tid in thread_status:
+        thread_status[tid] = {'url':None, 'proxy':None, 'succeeded':0, 'new_urls_count':0, 'fetched_size':0}
+
+    # URL currently being fetched
+    thread_status[tid]['url'] = url
+    thread_status[tid]['proxy'] = proxy
+    lock.release()
+
     with Database(DB_URL) as db:
         document = db.fetch_document(url)
         has_url = (document != None)
 
         request_succeeded = 0
-        new_urls_count = 0
-
-        pxy = random.choice(proxy_list)
-        proxy = Proxy(pxy[0], pxy[1], pxy[2])
+        new_urls_count = 0        
 
         if document == None or document.content == None:
-            print "Th%d: Fetching %s via %s" % (thread_seq, url, proxy)
+            #print "Th%d: Fetching %s via %s" % (thread_seq, url, proxy)
             task = FetchTask(url)
             try:
                 document = task.run(proxy, db)
@@ -47,7 +86,7 @@ def fetch_url(url, thread_seq=0):
                     urls = document.extract_urls(url_pattern)
                     new_urls_count += len(urls)
                     db.insert_urls(urls)
-                print "Th:%d: Found %d URLs in %s." % (thread_seq, new_urls_count, url)
+                #print "Th:%d: Found %d URLs in %s." % (thread_seq, new_urls_count, url)
 
             except urllib2.URLError as e:
                 print 'URLError has been raised. Probably a proxy problem (%s).' % proxy
@@ -63,21 +102,24 @@ def fetch_url(url, thread_seq=0):
         # number of bytes of the fetched document
         fetched_size = len(document.content) if document != None and document.content != None else 0
 
-        return {'succeeded':request_succeeded, 'new_urls_count':new_urls_count, 'fetched_size':fetched_size}
+        lock.acquire()
+        status['processed_urls_count'] += 1
+        thread_status[tid]['new_urls_count'] = new_urls_count
 
-def fetch_unfetched_urls(limit):
-    with Database(DB_URL) as db:
-        curs = db.cursor
-        curs.execute("SELECT url FROM document WHERE last_fetched IS NULL LIMIT ?", (limit,))
+        screen.clear()
         
-        return map(lambda u: u[0], curs.fetchall())
+        for key in thread_status:
+            screen.addstr("[%s] " % key)
+            screen.addstr("Fetching %s via %s" % (thread_status[key]['url'][:40], thread_status[key]['proxy']))
+            screen.addstr("\n")
 
-def fetch_urls(urls, urls_count, thread_seq):
+        screen.addstr("(%d/%d)\n" % (status['processed_urls_count'], len(unfetched_urls)))
 
-    for url in urls:
-        #print 'Th%d: (%d/%d)' % (thread_seq, len(urls), urls_count)
-        fetch_url(url, thread_seq)
+        screen.refresh()
+    
+        lock.release()
 
+        return {'succeeded':request_succeeded, 'new_urls_count':new_urls_count, 'fetched_size':fetched_size}
 
 def reduce_report(row1, row2):
     return {'succeeded':row1['succeeded']+row2['succeeded'],
@@ -85,28 +127,30 @@ def reduce_report(row1, row2):
             'fetched_size':row1['fetched_size']+row2['fetched_size']}
 
 def main():
+    # stops curses from outputting key presses from the user onto the screen
+    curses.noecho() 
 
-    # number of processes
-    n_proc = 48
+    # removes the cursor from the screen
+    curses.curs_set(0)
 
-    # number of urls to fetch
-    n_urls = 10000
-
-    urls = fetch_unfetched_urls(n_urls)
+    # sets the mode which the screen uses when capturing key presses 
+    screen.keypad(1)
 
     pool = ThreadPool(n_proc)
-    result = pool.map(fetch_url, urls)
+    result = pool.map(fetch_url, unfetched_urls)
     report = reduce(reduce_report, result)
 
     with Database(DB_URL) as db:
         url_count = db.url_count
         fetched_url_count = db.fetched_url_count
 
+        curses.endwin()
+
         print
         print "-[ Spider Report: This session ]------------------------------------"
-        print "  Number of fetch requests sent out: %d" % (len(urls))
+        print "  Number of fetch requests sent out: %d" % (len(unfetched_urls))
         print "  Number of successful fetches: %s" % report['succeeded']
-        print "  Live proxy hit ratio: %.02f%%" % (100.0 * report['succeeded'] / len(urls))
+        print "  Live proxy hit ratio: %.02f%%" % (100.0 * report['succeeded'] / len(unfetched_urls))
         print "  Sum of size of fetched documents: %d" % report['fetched_size']
         print "  Number of newly found URLs: %d" % report['new_urls_count']
         print
@@ -117,7 +161,13 @@ def main():
 
 if __name__ == '__main__':
     proxy_list = load_proxy_list("proxy_list.txt")
-    main()
+    try:
+        main()
+    except:
+        curses.endwin()
+        import traceback
+        traceback.print_exc()
+
     #url = "http://messages.finance.yahoo.com/Business_%26_Finance/Investments/Stocks_%28A_to_Z%29/Stocks_J/threadview?bn=10073&tid=443633&mid=443634"
     #url ="http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_A/threadview?m=tm&bn=1028&tid=1447176&mid=1447176&tof=35&rt=2&frt=2&off=1"
     #fetch_url("http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_M/threadview?m=ts&bn=12004&tid=1800636&mid=1800636&tof=1&frt=2")
--- spider.py
+++ spider.py
@@ -59,46 +59,6 @@ class FetchTask:
         return Document(url, None, datetime.datetime.now(), content)
 
 
-class TaskDispatcher:
-    def dispatch(self, task):
-        """
-        task -- an instance of FetchTask
-        """
-        raise Exception('Not implemented')
-
-
-class Task(Thread):
-    """Thread executing tasks from a given tasks queue.
-    http://code.activestate.com/recipes/577187-python-thread-pool/
-    """
-    def __init__(self, tasks):
-        Thread.__init__(self)
-        self.tasks = tasks
-        self.daemon = True
-        self.start()
-    
-    def run(self):
-        while True:
-            func, args, kargs = self.tasks.get()
-            try: func(*args, **kargs)
-            except Exception, e: print e
-            self.tasks.task_done()
-
-class ThreadPool2:
-    """Pool of threads consuming tasks from a queue"""
-    def __init__(self, num_threads):
-        self.tasks = Queue(num_threads)
-        for _ in range(num_threads): Worker(self.tasks)
-
-    def add_task(self, func, *args, **kargs):
-        """Add a task to the queue"""
-        self.tasks.put((func, args, kargs))
-
-    def wait(self):
-        """Wait for completion of all the tasks in the queue"""
-        self.tasks.join()
-
-
 class Document:
 
     # FIXME: This does not cover all possible forms of URLs, but we'll
@@ -116,50 +76,6 @@ class Document:
 
     def __setstate__(self, dict):
         self.__dict__.update(dict)
-
-    def url():
-        doc = "The url property."
-        def fget(self):
-            return self._url
-        def fset(self, value):
-            self._url = value
-        def fdel(self):
-            del self._url
-        return locals()
-    url = property(**url())
-
-    def mime_type():
-        doc = "The mime_type property."
-        def fget(self):
-            return self._mime_type
-        def fset(self, value):
-            self._mime_type = value
-        def fdel(self):
-            del self._mime_type
-        return locals()
-    mime_type = property(**mime_type())
-
-    def last_fetched():
-        doc = "The last_fetched property."
-        def fget(self):
-            return self._last_fetched
-        def fset(self, value):
-            self._last_fetched = value
-        def fdel(self):
-            del self._last_fetched
-        return locals()
-    last_fetched = property(**last_fetched())
-
-    def content():
-        doc = "The content property."
-        def fget(self):
-            return self._content
-        def fset(self, value):
-            self._content = value
-        def fdel(self):
-            del self._content
-        return locals()
-    content = property(**content())
 
     def extract_urls(self, url_pattern=None):
         """Returns a list of HTTP/S URLs in string format."""
