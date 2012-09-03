66fd4239582eaf08138bd17392d8a1769a00f831
Sumin Byeon <suminb@gmail.com>
2012-09-02 22:47:38 -0700

    Revised URL_PATTERNS.

--- patterns.py
+++ patterns.py
@@ -4,7 +4,7 @@ URL_PATTERNS = (
 	r"http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_M/forumview\?bn=12004[\/\w %=;&\.\-\+\?]*\/?",
 
 	# Microsoft (single topic)
-	r"http://messages.finance.yahoo.com/Business_%26_Finance/Investments/Stocks_%28A_to_Z%29/Stocks_M/threadview\?[\/\w %=;&\.\-\+\?]*bn=12004[\/\w %=;&\.\-\+\?]*\/?",
+	r"http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_M/threadview\?[\/\w %=;&\.\-\+\?]*bn=12004[\/\w %=;&\.\-\+\?]*\/?",
 
 	# Microsoft (all messages)
 	r"http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_M/messagesview\?bn=12004[\/\w %=;&\.\-\+\?]*\/?",
