630548b87b6dd35a043271a06512aa19e4d46538
Sumin Byeon <suminb@gmail.com>
2012-09-05 01:52:30 -0700

    Testing with different URL patterns.

--- patterns.py
+++ patterns.py
@@ -1,16 +1,5 @@
 URL_PATTERNS = (
 
-	# Microsoft (all topics)
-	r"http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_M/forumview\?bn=12004[\/\w %=;&\.\-\+\?]*\/?",
+	r"https?://finance.yahoo.com/[\/\w %=;&\.\-\+\?]*bn=30ec85b4-d1ce-32fe-a71c-f56c94fe758b[\/\w %=;&\.\-\+]*\/?",
 
-	# Microsoft (single topic)
-	r"http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_M/threadview\?[\/\w %=;&\.\-\+\?]*bn=12004[\/\w %=;&\.\-\+\?]*\/?",
-
-	# Microsoft (all messages)
-	r"http://messages.finance.yahoo.com/Stocks_%28A_to_Z%29/Stocks_M/messagesview\?bn=12004[\/\w %=;&\.\-\+]*\/?",
-
-	r"http://messages.finance.yahoo.com/[\/\w %=;&\.\-\+\?]*bn=26534[\/\w %=;&\.\-\+]*\/?",
-
-    #r"http://messages.finance.yahoo.com/[A-Z][\/\w %=;&\.\-\+\?]*\/?",
-    r"http://messages.finance.yahoo.com/search[\/\w %=;&\.\-\+\?]*\/?",
 )
